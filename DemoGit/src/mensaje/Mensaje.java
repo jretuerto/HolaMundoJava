6f633dd3fe841cc7038d4cb8f4c6e8d7712bcfd0
ciclope <ciclope@x-ciclope>
2017-03-11 01:56:18 -0500

    Borrar linea

--- DemoGit/src/mensaje/Mensaje.java
+++ DemoGit/src/mensaje/Mensaje.java
@@ -3,8 +3,7 @@ package mensaje;
 public class Mensaje {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo!");
 	}
 
 }
